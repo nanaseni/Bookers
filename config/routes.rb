d70d4e60d3131481291a3c8e63736db6dbccfde8
EC2 Default User <ec2-user@ip-172-31-10-145.ec2.internal>
2022-11-30 14:41:53 +0000

    [update]中断

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
